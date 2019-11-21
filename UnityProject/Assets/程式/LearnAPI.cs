7cbc10c2897ee2d8924552c9155498835908d9e1
you92524 <57396776+you92524@users.noreply.github.com>
2019-11-21 21:35:20 +0800

    水管延遲生成完成

---
+++ UnityProject/Assets/程式/LearnAPI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    private void Start()
+    {
+        print("輸出");
+
+        Debug.Log("輸出訊息");
+
+        //靜態成員:屬性的取得方式
+        print(Random.value);
+
+        //練習取得 PI
+        print(Mathf.PI);
+
+        // 靜態方法：類別.方法(對應的參數);
+        print("隨機範圍:" + Random.Range( 10.5f,11.5f));
+
+        //object 任何類型:int, float,string,bool
+        Debug.LogError("出錯啦");
+        Debug.LogWarning("在改剁手");
+    }
+
+    private void Update()
+    {
+        //打印出   輸入的按鍵   的字串
+        print(Input.inputString);
+
+        //練習:請練習偵測玩家有沒有按S
+        print("按下S鍵:"+Input.GetKeyDown("s"));
+    }
+}
--- UnityProject/Assets/程式/gamemanager.cs
+++ UnityProject/Assets/程式/gamemanager.cs
@@ -6,6 +6,28 @@ public class gamemanager : MonoBehaviour
     public int now = 0;    //目前分數
     [Header("最高分數")]
     public int best = 0;   //最高分數
+    [Header("水管")]
+    public GameObject pipe;// GameObject :可以存放預製物
+
+    /// <summary>
+    /// 水管生成功能
+    /// </summary>
+    public void SpawnPipe()
+    {
+        //因為有繼承此類別才能簡寫
+        //object.Instantiate(pipe);
+
+        //浮點數 y = 隨機.靜態方法(最小,最大)
+        float y = Random.Range(-2.19f, 2.75f);
+        
+        //三維向量 Vetor3 - x,y,z
+        Vector3 pos = new Vector3(6f, y, 0);
+        
+        //四元數 Quaterion - x,y,z,w
+        Quaternion rot = new Quaternion(0, 0, 0, 0);
+
+        Instantiate(pipe,pos,rot);
+    }
 
     /// <summary>
     /// 加分功能
@@ -30,5 +52,12 @@ public class gamemanager : MonoBehaviour
     public void GameOver()
     {
 
+    }
+
+    private void Start()
+    {
+
+        //SpawnPipe();
+        InvokeRepeating("SpawnPipe", 0, 2f);
     }
 }
