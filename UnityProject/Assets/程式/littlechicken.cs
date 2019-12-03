fb9bed074e2cee1bd01e05671a0a8e394edf248a
you92524 <57396776+you92524@users.noreply.github.com>
2019-12-03 20:18:08 +0800

    小雞跳躍完成

--- UnityProject/Assets/程式/littlechicken.cs
+++ UnityProject/Assets/程式/littlechicken.cs
@@ -2,7 +2,7 @@
 
 public class littlechicken : MonoBehaviour
 {
-    [Header("跳越高度"),Range(1,100)]
+    [Header("跳越高度"),Range(100,1000)]
     [Tooltip("控制小雞的跳越高度，數值越大，越高")]
     public int jumphihnt=5;    //跳躍高度
 
@@ -10,6 +10,9 @@ public class littlechicken : MonoBehaviour
     public bool surive = true; //生存狀態 預設 活著
 
     public GameObject goscore, goGM;
+
+    [Header("剛體")]
+    public Rigidbody2D r2d;
 
     /// <summary>
     /// 小雞跳躍功能
@@ -25,7 +28,17 @@ public class littlechicken : MonoBehaviour
             goscore.SetActive(true);
             // 顯示/起動 遊戲管理員
             goGM.SetActive(true);
+
+            r2d.gravityScale = 1f;  // 剛體 重力=1
+
+            r2d.Sleep();            //剛體 睡覺
+
+            r2d.AddForce(new Vector2(0f,jumphihnt));    //剛體 增加推力(二維向量)
+
         }
+        //剛體.加速度velocity     (x,y)  x:左右    y:上下
+        r2d.SetRotation(4.95f*r2d.velocity.y);         //剛體.設定旋轉(浮點數_角度)
+
     }
 
     /// <summary>
